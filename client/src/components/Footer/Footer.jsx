cfbd01ce0611eeeb49698a1ec8726ed78bb15a13
Kevin J Medina <kevinfx@live.com>
2021-04-13 12:04:49 -0400

    Added a Responsive Portfolio Page and Routes that link pages together

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
 	return (
 		<footer
-			className="w-full h-full rounded-t-2xl bg-purple-500 text-gray-100 px-12 py-8  flex justify-start items-center space-x-12 md:px-24 md:py-12 xl:px-72">
+			className="w-full h-full rounded-t-2xl bg-purple-500 text-gray-100 px-12 py-8  flex justify-start items-start space-x-12 md:px-24 md:py-12 xl:px-72">
 			<ul>
 				<li className="footerListTitle">Local Links</li>
-				<li className="footerLink">Home</li>
-				<li className="footerLink">About</li>
-				<li className="footerLink">Contact</li>
-				<li className="footerLink">Portfolio</li>
+				<li className="footerLink"><Link to="/">Home</Link></li>
+				<li className="footerLink"><Link to="/portfolio">Portfolio</Link></li>
 			</ul>
 			<ul>
 				<li className="footerListTitle">Stack</li>
--- client/src/components/Header/Nav.jsx
+++ client/src/components/Header/Nav.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
-export default function Nav( { addNavClass } ) {
+export default function Nav( { addNavClass, setMenu } ) {
 	return (
 		<>
 			<nav className={`nav ${addNavClass}`}>
 				<ul className={`navList`}>
-					<li className={`navLink`}><Link to="/">Home</Link></li>
-					<li className={`navLink`}><Link to="/portfolio">Portfolio</Link></li>
+					<li className={`navLink`} onClick={() => setMenu(false)}><Link to="/">Home</Link></li>
+					<li className={`navLink`} onClick={() => setMenu(false)}><Link to="/portfolio">Portfolio</Link></li>
 				</ul>
 			</nav>
 		</>
