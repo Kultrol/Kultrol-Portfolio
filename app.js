0ac33d6b40f31be80a13910b03cf96ea915b9e3b
Kevin J Medina <kevinfx@live.com>
2021-04-15 14:21:53 -0400

    Added a GIT IGNORE

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.route( '/api' )
 	} )
 
 if(process.env.Node_ENV === 'production'){
-	app.use(express.static('../client/build'))
+	app.use(express.static('client/build'))
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve('../', 'client', 'build', 'index.html'))
 	})
